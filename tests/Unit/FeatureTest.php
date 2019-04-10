fb96002ea0b572b199f6931bb93d817362178f3f
sravya <saisravyam@gmail.com>
2019-04-10 17:58:52 -0400

    Created FeatureTest to check if the register return a 200 status code.

---
+++ tests/Unit/FeatureTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class FeatureTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testBasicTest()
+    {
+        $response = $this->get('/register');
+
+        $response->assertStatus(200);
+    }
+    public function loginTest()
+    {
+        $response = $this->get('/login');
+
+        $response->assertStatus(200);
+    }
+}
